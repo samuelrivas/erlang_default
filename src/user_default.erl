2ec2d235ec2e969b45f030005959851ec9f4ec06
Samuel Rivas <samuelrivas@gmail.com>
2012-06-16 14:34:51 +0200

    RESTRUCT delete old functions no longer used

--- src/user_default.erl
+++ src/user_default.erl
@@ -1,7 +1,6 @@
 -module(user_default).
 
--export([make/0, make/1, make_dir/1, make_dir/2, cd_test/1, cd_test/2, samp/1,
-         pp/1, catch_all/1]).
+-export([make/0, make/1, make_dir/1, make_dir/2, samp/1, pp/1, catch_all/1]).
 
 make() ->
     make([]).
@@ -20,22 +19,6 @@ make_dir(Dir, Options) ->
     after
         file:set_cwd(Old)
     end.
-
-cd_test(App) ->
-    cd_test(eqc, App).
-
-cd_test(Type, App) ->
-    file:set_cwd(
-      io_lib:format("~p/trunk/src/test/~s", [App, format_type(Type)])),
-    element(2, file:get_cwd()).
-
-format_type(eqc) ->
-    "quickcheck";
-format_type(eunit) ->
-    "eunit".
-
-samp(X) ->
-    eqc_gen:sample(X).
 
 pp(Term) ->
     io:format("~w~n", [Term]).
