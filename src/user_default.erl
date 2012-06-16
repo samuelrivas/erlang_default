d3bdaf07562170dca14e6cd9618a50ea1a0aafcd
Samuel Rivas <samuelrivas@gmail.com>
2012-06-16 14:25:07 +0200

    FUNCT add the catch_all function

--- src/user_default.erl
+++ src/user_default.erl
@@ -1,7 +1,7 @@
 -module(user_default).
 
 -export([make/0, make/1, make_dir/1, make_dir/2, cd_test/1, cd_test/2, samp/1,
-         pp/1]).
+         pp/1, catch_all/1]).
 
 make() ->
 	make([]).
@@ -39,3 +39,8 @@ samp(X) ->
 
 pp(Term) ->
     io:format("~w~n", [Term]).
+
+catch_all(Fun) ->
+    try Fun()
+    catch Type:Reason -> {Type, Reason}
+    end.
