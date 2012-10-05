e1bafcdb41b2e6cc3f6f60b65136bcab4a89b9e2
Samuel Rivas <samuelrivas@gmail.com>
2012-10-05 11:14:24 +0200

    FUNCT add src function
    Returns the source from which a module was compiled

--- src/user_default.erl
+++ src/user_default.erl
@@ -1,7 +1,7 @@
 -module(user_default).
 
 -export([make/0, make/1, make_dir/1, make_dir/2, samp/1, pp/1, pps/1,
-         catch_all/1]).
+         catch_all/1, src/1]).
 
 make() ->
     make([]).
@@ -40,3 +40,5 @@ catch_all(Fun) ->
     try Fun()
     catch Type:Reason -> {Type, Reason}
     end.
+
+src(Module) -> proplists:get_value(source, Module:module_info(compile)).
